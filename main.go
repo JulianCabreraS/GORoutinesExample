a4b347c74b22c8cc85846c4f8399d9cad325a538
jcabrera <julian.cabreras@gmail.com>
2022-06-04 15:42:36 -0500

    Example of Channel and GO Routines

---
+++ main.go
@@ -0,0 +1,43 @@
+//Example of Couroutines
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+	}
+
+	//Create a channel
+	c := make(chan string)
+
+	for _, link := range links {
+
+		go checkLink(link, c) // Coroutines
+	}
+	//Receiving the value from the channel
+	for l := range c {
+		time.Sleep(5 * time.Second)
+		go checkLink(l, c)
+	}
+
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up")
+	c <- link
+}
